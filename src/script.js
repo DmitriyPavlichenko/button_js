2b9c9a59c811832cb37cc636a12d2247298bf75a
Dmitriy Pavlichenko <pavlichenko.dmitriy@gmail.com>
2021-04-05 11:00:34 +0300

    inital commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+function printResult(numberSequence, sumOdd) {
+    document.getElementById("sequence").innerHTML = "Your sequence: " + numberSequence;
+    document.getElementById("sum").innerHTML = "Sum of odd numbers: " + sumOdd;
+}
+
+let button = document.querySelector("button");
+button.onclick = function () {
+    let step;
+    while(true) {
+        step = prompt('Write step: ', '5');
+        if (step >= 1 && step <= 50) {
+            alert("All is right. Result is near...");
+            break;
+        } else {
+            alert("Invalid input! Try again");
+        }
+    }
+
+    let numberSequence = "";
+    let sumOdd = 0;
+    let currentNumb;
+    for (let i = 1; i < step; i++) {
+        currentNumb = (i * i * i) * Math.pow(3, (2 * i));
+        if (currentNumb % 2 !== 0) {
+            numberSequence += currentNumb.toString().fontcolor("red") + " ";
+            sumOdd += currentNumb;
+            continue;
+        }
+        numberSequence += currentNumb + " ";
+    }
+
+    printResult(numberSequence, sumOdd);
+}
